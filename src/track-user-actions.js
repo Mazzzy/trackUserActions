8e0f1cdb6c5a29f352536ec6744d2cc5fc090fe0
Mazahar <mazahar@idyllic.co>
2017-11-11 14:32:51 +0530

    initial plugin js added

---
+++ src/track-user-actions.js
@@ -0,0 +1,26 @@
+/**
+ * A pure javascript based plugin
+ *
+ * Purpose: To track user's behavior events in any given page.
+ * It tracks clicks, mouse events, key events, time spent, node (html) click etc. 
+ *
+ * Developed by: Mazahar S. Shaikh (Github: @Mazzzy | twitter: @mazahar_shaikh)
+ */
+(function () {
+  // namespace to track user behavior
+  var trackUser = { };
+
+  // by default options
+
+  // available features 
+
+  // helper functions
+
+  // end results
+
+  // attaching stuffs on namespace
+
+  // exporting namespace via window
+  window.trackUser = trackUser;
+
+})();
